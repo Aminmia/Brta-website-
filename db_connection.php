3e95af17e70ef3b9fc799f21456ba4f5d2d9da86
Anisur Rahaman <60566098+Aminmia@users.noreply.github.com>
2023-12-21 00:35:56 +0600

    Add files via upload

---
+++ db_connection.php
@@ -0,0 +1,18 @@
+<?php
+
+function connectDB() {
+    $servername = "localhost";
+    $username = "root";
+    $password = "";
+    $dbname = "AnisurDB";
+
+    $conn = new mysqli($servername, $username, $password, $dbname);
+
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    }
+
+    return $conn;
+}
+
+?>
